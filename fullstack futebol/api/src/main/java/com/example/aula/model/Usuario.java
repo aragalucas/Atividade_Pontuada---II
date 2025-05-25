283d80cdb1b4134af5304964d3fe3dd2cc14687c
aragalucas <126295197+aragalucas@users.noreply.github.com>
2025-05-25 16:12:05 -0300

    Add files via upload

---
+++ fullstack futebol/api/src/main/java/com/example/aula/model/Usuario.java
@@ -0,0 +1,116 @@
+package com.example.aula.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório.")
+    private String nome;
+
+    @NotBlank(message = "Sexo obrigatório.")
+    private String sexo;
+
+    @NotBlank(message = "idade é obrigatória.")
+    private String idade;
+
+    @NotBlank(message = "altura é obrigatório.")
+    private String altura;
+
+    @NotBlank(message = "Peso é obrigatório.")
+    private String peso;
+
+    @NotBlank(message = "posicao é obrigatorio.")
+    private String posicao;
+
+    @NotBlank(message = "numero é obrigatorio.")
+    private String numero;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome, String email, String senha, String telefone, String cpf, String cep) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.posicao = posicao;
+        this.numero = numero;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public String getIdade() {
+
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+
+        this.idade = idade;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
